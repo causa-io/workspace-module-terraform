d274aabf4f8f4b45692e24131fa562c0f24cc62d
Flo <florent.vouin@gmail.com>
2023-05-18 08:04:42 +0200

    ✨ Implement the TerraformService

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { ModuleRegistrationFunction } from '@causa/workspace';
 
 export * from './configurations/index.js';
+export * from './services/index.js';
 
 const registerModule: ModuleRegistrationFunction = async () => {};
 
