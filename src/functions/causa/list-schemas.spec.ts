ab0ed167191ffd54193f7c2fcbbbd8c13103939c
Flo <florent.vouin@gmail.com>
2026-02-10 12:17:13 +0100

    ✨ Implement CausaListConfigurationSchemas for the Terraform module

---
+++ src/functions/causa/list-schemas.spec.ts
@@ -0,0 +1,19 @@
+import { CausaListConfigurationSchemas } from '@causa/workspace-core';
+import { createContext } from '@causa/workspace/testing';
+import 'jest-extended';
+import { basename } from 'path';
+import { CausaListConfigurationSchemasForTerraform } from './list-schemas.js';
+
+describe('CausaListConfigurationSchemasForTerraform', () => {
+  it('should return the configuration schemas for the Terraform module', async () => {
+    const { context } = createContext({
+      configuration: { workspace: { name: 'test' } },
+      functions: [CausaListConfigurationSchemasForTerraform],
+    });
+
+    const actualSchemas = await context.call(CausaListConfigurationSchemas, {});
+
+    const actualBaseNames = actualSchemas.map((s) => basename(s));
+    expect(actualBaseNames).toIncludeSameMembers(['terraform.yaml']);
+  });
+});
